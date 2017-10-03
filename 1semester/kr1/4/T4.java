5b34d3db92a8cef8ae647bdae00156f9e3266efa
borisgk98 <borisgk98@ya.ru>
2017-10-03 13:22:53 +0000

    Add readme file to kr1

--- 1semester/kr1/4/T4.java
+++ 1semester/kr1/4/T4.java
@@ -2,14 +2,15 @@ import java.lang.Math;
 
 class T4 {
 	public static void main(String[] args) {
-		if (args.length < 2) {
+		if (args.length < 3) {
 			System.err.println("Bad args");
 			System.exit(1);
 		}
-		int n = Integer.parseInt(args[0]), m = Integer.parseInt(args[1]);
+		int n = Integer.parseInt(args[0]), m = Integer.parseInt(args[1]), scale = Integer.parseInt(args[2]);
 		for (int y = m; y >= 0; y--) {
+			System.out.printf("%3d", y);
 			for (int x = 0; x <= n; x++) {
-				if (y == Math.round(Math.sin(x)) + 10) {
+				if (y == scale * Math.round(Math.sin(x / scale)) + scale * 10) {
 					System.out.print('*');
 				}
 				else {
